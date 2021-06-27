1aa71f0d6a947eebfb2fdbc886dd73d75dcd1d38
Your Name <cris1945215@gmail.com>
2021-06-28 00:33:57 +0900

    생성자 DI

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -3,6 +3,7 @@ package spring.di;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -14,6 +15,7 @@ public class Program {
 		
 		/*	스프링에게 지시하는 방법으로 코드를 변경
 		Exam exam = new NewlecExam();
+		Exam exam = new NewlecExam(10,10,10,10);
 		ExamConsole console = new GridExamConsole();
 		
 		console.setExam(exam);
@@ -22,6 +24,8 @@ public class Program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		ExamConsole console =(ExamConsole) context.getBean("console");
 //		ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
