3b78a556b2338eb64111aaf036b186eb0e85b652
Your Name <cris1945215@gmail.com>
2021-07-05 22:59:14 +0900

    newlec part2 AOP 4

--- src/spring/aop/Program.java
+++ src/spring/aop/Program.java
@@ -4,6 +4,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.aop.entity.Exam;
 import spring.aop.entity.NewlecExam;
 
@@ -11,6 +14,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		ApplicationContext context = 
+//				new AnnotationConfigApplicationContext(NewlecDIConfig.class);
+				new ClassPathXmlApplicationContext("spring/aop/setting.xml");
+		
+		Exam exam = (Exam) context.getBean("exam");
+		
+		System.out.printf("total is %d\n",exam.total());
+		System.out.printf("avg is %f\n",exam.avg());
+		
+		/*
 		Exam exam = new NewlecExam(1,1,1,1);
 		
 		Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
@@ -33,9 +46,9 @@ public class Program {
 					}
 				}
 				);
+		*/
 		
-		System.out.printf("total is %d\n",proxy.total());
-		System.out.printf("avg is %f\n",proxy.avg());
+
 	}
 
 }
